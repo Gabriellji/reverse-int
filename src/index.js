7373a3fd3cdd5896a4c0ea49723046fd1d3c0b85
Gabriellji <gabriellji@yahoo.com>
2020-03-23 19:41:15 +0300

    feat: done reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function reverse (n) {
-  
+        if (n < 0) {
+            return (
+                parseFloat(
+                n
+                    .toString()
+                    .split('')
+                    .reverse()
+                    .join('')
+                ) * Math.sign(-n)
+            ) 
+
+        } else {
+            return (
+                parseFloat(
+                  n
+                    .toString()
+                    .split('')
+                    .reverse()
+                    .join('')
+                ) * Math.sign(n)
+              ) 
+        }  
 }
